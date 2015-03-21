1001a85e2fcff9f55f047b4fbb470e2b0f305cf9
Anko painting <anko.com@gmail.com>
2015-03-21 10:23:21 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationTests::Application.config.secret_token = '719bc41a7dfb1076936958290c789b9c3abb193f36162e6e3d90f12eff761f0ea8cabb2503b8fcb0b2aaca0871e5868130097279c2296581e49660763c5ad6aa'
